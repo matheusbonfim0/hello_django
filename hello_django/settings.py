4df8582b988a4c60cce111a55aaca0ea5e8b51db
Matheus Bonfim <math.bonfim1@gmail.com>
2021-11-17 21:17:40 -0400

    Removendo o secret_key

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y^fg)470^nz0$e!9)e*-v*kkwy7(d)5)!h_&e^hb)^u__5qnp8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
